eb34c5571f21b83553b49a8edb5eb24f529ba3d5
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-23 07:53:04 +0000

    Finalize build-specific metadata for 022-A

--- apps/web/app/100-builds/022/a/page.tsx
+++ apps/web/app/100-builds/022/a/page.tsx
@@ -1,4 +1,6 @@
-import {buildVariantMetadata} from '../../_metadata';\nexport const metadata=buildVariantMetadata('022','a');\nimport type {Metadata} from 'next';
+import {buildVariantMetadata} from '../../_metadata';
+export const metadata=buildVariantMetadata('022','a');
+import type {Metadata} from 'next';
 import Link from 'next/link';
 import HumanConsequenceLab from './HumanConsequenceLab';
 import shell from '../../001/room.module.css';
