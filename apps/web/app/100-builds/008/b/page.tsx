1281b0140f0e7651dd71ba5c51ebfac068a75671
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-23 07:52:26 +0000

    Finalize build-specific metadata for 008-B

--- apps/web/app/100-builds/008/b/page.tsx
+++ apps/web/app/100-builds/008/b/page.tsx
@@ -1,4 +1,6 @@
-import {buildVariantMetadata} from '../../_metadata';\nexport const metadata=buildVariantMetadata('008','b');\nimport type {Metadata} from 'next';
+import {buildVariantMetadata} from '../../_metadata';
+export const metadata=buildVariantMetadata('008','b');
+import type {Metadata} from 'next';
 import Link from 'next/link';
 import DecisionPathField from './DecisionPathField';
 import shell from '../../001/room.module.css';
