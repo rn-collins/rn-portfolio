e975d1f5170627a61d2c69b46b30d6a2082be339
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-23 07:16:53 +0000

    Use build-scoped social metadata for 005-A

--- apps/web/app/100-builds/005/a/page.tsx
+++ apps/web/app/100-builds/005/a/page.tsx
@@ -1,4 +1,5 @@
-import type {Metadata} from 'next';
+import {buildVariantMetadata} from '../../_metadata';
+export const metadata=buildVariantMetadata('005','a');
 import Link from 'next/link';
 import ServiceSoftwareLab from './ServiceSoftwareLab';
 import shell from '../../001/room.module.css';
