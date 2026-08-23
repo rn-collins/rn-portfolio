093336e17fef0700f379a5ba5ea721e4bb0fe812
rn-collins <rayven.nikkita.collins@gmail.com>
2026-08-23 07:52:22 +0000

    Finalize build-specific metadata for 004-A

--- apps/web/app/100-builds/004/a/page.tsx
+++ apps/web/app/100-builds/004/a/page.tsx
@@ -1,4 +1,6 @@
-import {buildVariantMetadata} from '../../_metadata';\nexport const metadata=buildVariantMetadata('004','a');\nimport type {Metadata} from 'next';
+import {buildVariantMetadata} from '../../_metadata';
+export const metadata=buildVariantMetadata('004','a');
+import type {Metadata} from 'next';
 import Link from 'next/link';
 import ManualIntelligenceEngine from './ManualIntelligenceEngine';
 import s from '../../001/room.module.css';
